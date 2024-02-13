fe37e2cd23501815b782cc595252a1e334bb29c9
bertars <bertars80@yahoo.es>
2024-02-13 16:35:40 +0100

    Testing baseline

--- TrainTicket/execute_baseline.sh
+++ TrainTicket/execute_baseline.sh
@@ -1,41 +1,52 @@
-RUN_DURATION=600 # 10 minutes
-COOLDOWN_PERIOD=300 # 5 minutes
+RUN_DURATION=60 # 10 minutes
+COOLDOWN_PERIOD=30 # 5 minutes
 RUNS_PER_CONFIGURATION=8
+CONTAINER_START_TIMEOUT=20
+LOGS_EXTRACTION_TIMEOUT=10
 
 # Function to start data collection
 start_data_collection() {
     echo "Starting data collection for $1 at $(date "+%T.%6N")"
     # Start Scaphandre for power monitoring
-    scaphandre --exporter prometheus &> "scaphandre_$1.log" &
+    # scaphandre --exporter prometheus &> "logs/scaphandre_$1.log" &
     
     # Start WattsUpPro monitoring
-    echo python3 ../wattsuppro_logger/WattsupPro.py -l -o gl4.log -p /dev/ttyUSB1 > /dev/null 2>&1 &
+    # echo python3 ../wattsuppro_logger/WattsupPro.py -l -o gl4.log -p /dev/ttyUSB2 > /dev/null 2>&1 &
 
     
-    # Start network traffic capture
-    # tcpdump -i docker0 -w "network_$1.pcap" &
 }
 
 stop_data_collection() {
     echo "Stopping data collection at $(date "+%T.%6N")"
-    killall scaphandre
-    pkill -f ../wattsuppro_logger-main/WattsupPro.py
+    # killall scaphandre
+    # pkill -f ../wattsuppro_logger-main/WattsupPro.py
 }
 
+mkdir -p logs
 
 for run in $(seq 1 $RUNS_PER_CONFIGURATION); do
-    echo "Running baseline ${run} at $(date "+%T.%6N")"
-
-    start_data_collection "$baseline_run${run}"
-
     echo "Starting Docker environment with Zipkin configurations and microservices at $(date "+%T.%6N")"
     docker-compose -f deployment/baseline/docker-compose-baseline.yml --env-file .env up -d
 
+    echo "Waiting for services to start"
+    sleep $CONTAINER_START_TIMEOUT
+    echo "Services started!"
+
+    executionStartTime=$(date "+%T.%6N")
+    echo "Running baseline ${run} at $(date "+%T.%6N")"
+    
+    start_data_collection "$baseline_run${run}"
+
     sleep $RUN_DURATION
 
-    docker-compose down
-
     stop_data_collection
+
+    executionStopTime=$(date "+%T.%6N")
+    echo "Stopped baseline at $executionStopTime"
+    echo "baseline, run, $executionStartTime, $executionStopTime" >> logs/baseline_log_${run}.csv
+
+    echo "Stopping services"
+    docker-compose -f deployment/baseline/docker-compose-baseline.yml --env-file .env down
 
     echo "Cooling down at $(date "+%T.%6N")"
     sleep $COOLDOWN_PERIOD
