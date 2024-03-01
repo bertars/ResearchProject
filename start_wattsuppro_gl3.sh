8d54024166c2d8c4bf82879481d91dec4a4b2cef
bertars <bertars80@yahoo.es>
2024-03-01 10:59:03 +0000

    Executing WattsupPro in GL3

--- start_wattsuppro_gl3.sh
+++ start_wattsuppro_gl3.sh
@@ -1,23 +1,26 @@
-RUN_DURATION=60 # 10 minutes
-COOLDOWN_PERIOD=30 # 5 minutes
+RUN_DURATION=600 # 10 minutes
+COOLDOWN_PERIOD=300 # 5 minutes
 RUNS_PER_CONFIGURATION=8
 CONTAINER_START_TIMEOUT=20
-CONTAINER_STOP_TIMEOUT=10
-LOGS_EXTRACTION_TIMEOUT=10
-
-echo "Waiting for services to start"
-sleep $CONTAINER_START_TIMEOUT
+CONTAINER_STOP_TIMEOUT=15
+EXTRA_LOGGING_TIME=20
+SudoPassword=$(echo $(head -n 5 ./TrainTicket/.env | tail -1 | cut -d '=' -f 2))
+LoggingDuration=$(( ($CONTAINER_START_TIMEOUT + $RUN_DURATION + $CONTAINER_STOP_TIMEOUT + $COOLDOWN_PERIOD + $EXTRA_LOGGING_TIME) * $RUNS_PER_CONFIGURATION)) 
+env_file="baseline"
+# echo "Waiting for services to start"
+# sleep $CONTAINER_START_TIMEOUT
 
 loggingStartTime=$(date "+%T.%6N")
 echo "Monitoring baseline 1 at $loggingStartTime"
 
-sudo python3 ./wattsuppro_logger-main/WattsupPro.py -l -o gl3_baseline1.log -p /dev/ttyUSB0 -t $RUN_DURATION
+echo $SudoPassword | sudo -S python3 ./wattsuppro_logger-main/WattsupPro.py -l -o gl3_$env_file.log -p /dev/ttyUSB0 -t $LoggingDuration
 
-echo "Waiting for services to start"
-sleep $CONTAINER_STOP_TIMEOUT
+echo "Finished monitoring!"
+# echo "Waiting for services to stop"
+# sleep $CONTAINER_STOP_TIMEOUT
 
-echo "Cooling down at $(date "+%T.%6N")"
-sleep $COOLDOWN_PERIOD
+# echo "Cooling down at $(date "+%T.%6N")"
+# sleep $COOLDOWN_PERIOD
 
 # for run in $(seq 1 $RUNS_PER_CONFIGURATION); do
 #     echo "Waiting for services to start"
