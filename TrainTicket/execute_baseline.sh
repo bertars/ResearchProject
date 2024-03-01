2989005f89c8631283146554b72d114ed1f35bec
bertars <bertars80@yahoo.es>
2024-03-01 11:59:32 +0100

    Executing experiment and getting logs

--- TrainTicket/execute_baseline.sh
+++ TrainTicket/execute_baseline.sh
@@ -1,87 +1,53 @@
-RUN_DURATION=60 # 10 minutes
-COOLDOWN_PERIOD=30 # 5 minutes
+RUN_DURATION=600 # 10 minutes
+COOLDOWN_PERIOD=300 # 5 minutes
 RUNS_PER_CONFIGURATION=8
-CONTAINER_START_TIMEOUT=20
-LOGS_EXTRACTION_TIMEOUT=10
+
 
 # Function to start data collection
 start_data_collection() {
-    echo "Starting data collection for baseline at $(date "+%T.%6N")"
-    # Start Scaphandre for power monitoring
-    # docker run -d --name scaphandre_1 \
-    #     -v /sys/class/powercap:/sys/class/powercap \
-    #     -v /proc:/proc \
-    #     -v /var/run/docker.sock:/var/run/docker.sock \
-    #     --privileged -ti hubblo/scaphandre json -t $RUN_DURATION >> "logs/scaphandre_1.log" 
-    docker run --name scaphandre_1 -v /sys/class/powercap:/sys/class/powercap -v /proc:/proc -v /var/run/docker.sock:/var/run/docker.sock --privileged -ti hubblo/scaphandre json -t $RUN_DURATION --containers >> "logs/scaphandre_1.json" 
-    
-    
+    echo "Starting data collection for $1 at $(date "+%T.%6N")"
+    echo "Running Scaphandre..."
+    docker run --name scaphandre_$1 -v /sys/class/powercap:/sys/class/powercap -v /proc:/proc -v /var/run/docker.sock:/var/run/docker.sock --privileged -ti hubblo/scaphandre json -t $RUN_DURATION --containers >> "logs/scaphandre_$1.log"  
 }
 
-stop_data_collection() {
-    echo "Stopping data collection at $(date "+%T.%6N")"
-    docker stop scaphandre_1
-    docker rm scaphandre_1
-}
+# stop_data_collection() {
+#     echo "Stopping data collection at $(date "+%T.%6N")"
+#     docker stop scaphandre_1
+#     docker rm scaphandre_1
+# }
 
 mkdir -p logs
 
-echo "Starting Docker environment with Zipkin configurations and microservices at $(date "+%T.%6N")"
-docker compose -f deployment/baseline/docker-compose-baseline.yml --env-file .env up -d
 
-# echo "Waiting for services to start"
-# sleep $CONTAINER_START_TIMEOUT
-echo "Services started!"
+for run in $(seq 1 $RUNS_PER_CONFIGURATION); do
+    echo "Starting Docker environment with Zipkin configurations and microservices at $(date "+%T.%6N")"
+    # docker compose -f deployment/baseline/docker-compose-baseline.yml --env-file .env up -d
+    docker compose -f deployment/baseline/docker-compose-baseline.yml --env-file .env start
 
-executionStartTime=$(date "+%T.%6N")
-echo "Running baseline 1 at $executionStartTime"
+    # echo "Waiting for services to start"
+    # sleep $CONTAINER_START_TIMEOUT
+    echo "Services started!"
 
-start_data_collection "$baseline_run1"
+    executionStartTime=$(date "+%T.%6N")
+    echo "Running baseline ${run} at $executionStartTime"
 
-curl localhost:8080 > /dev/null 2>&1 &
+    curl localhost:8080 > /dev/null 2>&1 &
 
-echo "Sleeping..."
-sleep $RUN_DURATION
+    start_data_collection "$baseline_run${run}"
 
-stop_data_collection
+    # stop_data_collection
 
-executionStopTime=$(date "+%T.%6N")
-echo "Stopped baseline at $executionStopTime"
-echo "baseline, run1, $executionStartTime, $executionStopTime" >> logs/baseline_log_1.csv
+    executionStopTime=$(date "+%T.%6N")
+    echo "Stopped baseline at $executionStopTime"
+    echo "baseline, run${run}, $executionStartTime, $executionStopTime" >> logs/baseline.csv
 
-echo "Stopping services"
-docker compose -f deployment/baseline/docker-compose-baseline.yml --env-file .env down
+    echo "Stopping services"
+    docker compose -f deployment/baseline/docker-compose-baseline.yml --env-file .env stop
+    # docker compose -f deployment/baseline/docker-compose-baseline.yml --env-file .env down
 
-echo "Cooling down at $(date "+%T.%6N")"
-sleep $COOLDOWN_PERIOD
-
-# for run in $(seq 1 $RUNS_PER_CONFIGURATION); do
-#     echo "Starting Docker environment with Zipkin configurations and microservices at $(date "+%T.%6N")"
-#     docker-compose -f deployment/baseline/docker-compose-baseline.yml --env-file .env up -d
-
-#     echo "Waiting for services to start"
-#     sleep $CONTAINER_START_TIMEOUT
-#     echo "Services started!"
-
-#     executionStartTime=$(date "+%T.%6N")
-#     echo "Running baseline ${run} at $(date "+%T.%6N")"
-    
-#     start_data_collection "$baseline_run${run}"
-
-#     sleep $RUN_DURATION
-
-#     stop_data_collection
-
-#     executionStopTime=$(date "+%T.%6N")
-#     echo "Stopped baseline at $executionStopTime"
-#     echo "baseline, run${run}, $executionStartTime, $executionStopTime" >> logs/baseline_log_${run}.csv
-
-#     echo "Stopping services"
-#     docker-compose -f deployment/baseline/docker-compose-baseline.yml --env-file .env down
-
-#     echo "Cooling down at $(date "+%T.%6N")"
-#     sleep $COOLDOWN_PERIOD
-# done
+    echo "Cooling down at $(date "+%T.%6N")"
+    sleep $COOLDOWN_PERIOD
+done
 
 
 echo "Baseline completed!"
--- TrainTicket/execute_experiment.sh
+++ TrainTicket/execute_experiment.sh
@@ -2,45 +2,77 @@ RUN_DURATION=600 # 10 minutes
 COOLDOWN_PERIOD=300 # 5 minutes
 RUNS_PER_CONFIGURATION=8
 combinations_dir="env_files"
+env_file="combination_27.env"
 
 # Function to start data collection
 start_data_collection() {
     echo "Starting data collection for $1 at $(date "+%T.%6N")"
-    # Start Scaphandre for power monitoring
-    scaphandre --exporter prometheus &> "scaphandre_$1.log" &
-    
-    # Start WattsUpPro monitoring
-    echo python3 ../wattsuppro_logger/WattsupPro.py -l -o gl4.log -p /dev/ttyUSB1 > /dev/null 2>&1 &
-
-    
-    # Start network traffic capture
-    # tcpdump -i docker0 -w "network_$1.pcap" &
+    echo "Running Scaphandre..."
+    docker run --name scaphandre_$1 -v /sys/class/powercap:/sys/class/powercap -v /proc:/proc -v /var/run/docker.sock:/var/run/docker.sock --privileged -ti hubblo/scaphandre json -t $RUN_DURATION --containers >> "logs/scaphandre_$1.log"
 }
 
-stop_data_collection() {
-    echo "Stopping data collection at $(date "+%T.%6N")"
-    killall scaphandre
-    pkill -f ../wattsuppro_logger-main/WattsupPro.py
-}
+# stop_data_collection() {
+#     echo "Stopping data collection at $(date "+%T.%6N")"
+#     docker stop scaphandre_1
+#     docker rm scaphandre_1
+# }
+for run in $(seq 1 $RUNS_PER_CONFIGURATION); do
+    echo "Starting Docker environment with Zipkin configurations and microservices at $(date "+%T.%6N")"
+    # docker compose -f deployment/zipkin/docker-compose-zipkin.yml --env-file "env_files/combination_2.env" up -d
+    docker compose -f deployment/zipkin/docker-compose-zipkin.yml --env-file "${combinations_dir}/${env_file}" start
 
-for env_file in $(ls $combinations_dir); do
-    for run in $(seq 1 $RUNS_PER_CONFIGURATION); do
-        echo "Running ${env_file} iteration ${run} at $(date "+%T.%6N")"
+    echo "Services started!"
 
-        start_data_collection "${env_file}_run${run}"
+    executionStartTime=$(date "+%T.%6N")
+    echo "Running ${env_file} iteration ${run} at $executionStartTime"
 
-        echo "Starting Docker environment with Zipkin configurations and microservices at $(date "+%T.%6N")"
-        docker-compose -f deployment/zipkin/docker-compose-zipkin.yml --env-file "${combinations_dir}/${env_file}" up -d
+    curl localhost:8080 > /dev/null 2>&1 &
+    curl localhost:9411/zipkin > /dev/null 2>&1 &
 
-        sleep $RUN_DURATION
+    start_data_collection "${env_file}_run${run}"
 
-        docker-compose down
+    # stop_data_collection
 
-        stop_data_collection
+    executionStopTime=$(date "+%T.%6N")
+    echo "Stopped ${env_file}${run} at $executionStopTime"
+    echo "${env_file}, run${run}, $executionStartTime, $executionStopTime" >> logs/${env_file}.csv
 
-        echo "Cooling down at $(date "+%T.%6N")"
-        sleep $COOLDOWN_PERIOD
-    done
+    echo "Stopping services"
+    docker compose -f deployment/zipkin/docker-compose-zipkin.yml --env-file "${combinations_dir}/${env_file}" stop
+    # docker compose -f deployment/zipkin/docker-compose-zipkin.yml --env-file "${combinations_dir}/${env_file}" down
+
+    echo "Cooling down at $(date "+%T.%6N")"
+    sleep $COOLDOWN_PERIOD
 done
+# for env_file in $(ls $combinations_dir); do
+#     for run in $(seq 1 $RUNS_PER_CONFIGURATION); do
+#         echo "Starting Docker environment with Zipkin configurations and microservices at $(date "+%T.%6N")"
+#         # docker compose -f deployment/zipkin/docker-compose-zipkin.yml --env-file "${combinations_dir}/${env_file}" up -d
+#         docker compose -f deployment/zipkin/docker-compose-zipkin.yml --env-file "${combinations_dir}/${env_file}" start
+
+#         echo "Services started!"
+
+#         executionStartTime=$(date "+%T.%6N")
+#         echo "Running ${env_file} iteration ${run} at $executionStartTime"
+
+#         curl localhost:8080 > /dev/null 2>&1 &
+#         curl localhost:9411/zipkin > /dev/null 2>&1 &
+        
+#         start_data_collection "${env_file}_run${run}"
+
+#         stop_data_collection
+
+#         executionStopTime=$(date "+%T.%6N")
+#         echo "Stopped ${env_file}${run} at $executionStopTime"
+#         echo "${env_file}, run${run}, $executionStartTime, $executionStopTime" >> logs/${env_file}_log_${run}.csv
+
+#         echo "Stopping services"
+#         docker compose -f deployment/zipkin/docker-compose-zipkin.yml --env-file "${combinations_dir}/${env_file}" stop
+#         # docker compose -f deployment/zipkin/docker-compose-zipkin.yml --env-file "${combinations_dir}/${env_file}" down
+
+#         echo "Cooling down at $(date "+%T.%6N")"
+#         sleep $COOLDOWN_PERIOD
+#     done
+# done
 
 echo "Experiment completed!"
